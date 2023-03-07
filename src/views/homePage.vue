141e7f4fcf341af754ba61b0cd918cc072e76d85
yqliu <3471966565@qq.com>
2023-03-07 17:53:37 +0800

    544:

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,5 +1,16 @@
 <template>
-<div></div>
+<div>
+  <header>
+    <h1>
+      大数据
+    </h1>
+  </header>
+  <section class="container">
+    <section class="itemleft"></section>
+    <section class="itemcenter"></section>
+    <section class="itemright"></section>
+  </section>
+</div>
 </template>
 
 <script>
@@ -8,6 +19,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less">
+header{
+  height: 1rem;
+  width: 100%;
+  h1{
+    font-size: 0.375rem;
+    text-align: center;
+    line-height: 1rem;
+  }
 
+}
+.container{
+  min-width: 1200px;
+  max-width: 2048px;
+  margin: 0 auto;
+  border: 1px red;
+  padding: .125rem .125rem 0;
+  display: flex;
+  .itemleft,.itemright{
+    flex: 3;
+  }
+  .itemcenter{
+    flex: 5;
+  }
+}
 </style>
